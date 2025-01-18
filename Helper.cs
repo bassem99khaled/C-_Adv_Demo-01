40c62654457cb8aa69ac7f69ec18f7de6b8a7970
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-19 01:52:35 +0200

    SWAP Example

---
+++ Helper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Adv_Demo_01
+{
+    internal static class Helper
+    {
+
+        public static void SWAP<T>(ref T X, ref T  Y) /*where T : struct*/
+        {
+            Console.WriteLine("===== SWAP ======");
+            T Temp = X;
+            X = Y;
+            Y = Temp;
+        }
+
+        public static void Print(string Name)
+        {
+            Console.WriteLine($"Hello {Name}!!");
+        }
+
+        // public static void SWAP(ref object X , ref object Y)
+        // {
+        //     Console.WriteLine("===== SWAP ======");
+        //     object Temp = X;
+        //     X = Y;
+        //     Y=Temp;
+        // }
+
+        public static void SWAP(ref Point X, ref Point Y)
+        {
+            Console.WriteLine("===== SWAP ======");
+            Point Temp = X;
+            X = Y;
+            Y = Temp;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region SWAP Example
+            
+            ///  int A = 3, B = 5; // Boxing
+            ///  Console.WriteLine($"A = {A}");
+            ///  Console.WriteLine($"B = {B}");
+            ///  Helper.SWAP<int>(ref A,ref B );
+            ///  Console.WriteLine($"A={A}");
+            ///  Console.WriteLine($"B={B}");
+
+
+            /// double K = 3.3, L = 4.4;
+            /// Console.WriteLine($"K = {K}");
+            /// Console.WriteLine($"L = {L}");
+            ///  Helper.SWAP(ref K,ref L );
+            ///  Console.WriteLine($"k = {K}");
+            ///  Console.WriteLine($"L={L}");
+
+
+            /// Point p01 = new Point(10, 20);
+            /// Point p02 = new Point(60, 50);
+            /// Console.WriteLine($"P01 = {p01}");
+            /// Console.WriteLine($"P02 = {p02}");
+            ///
+            /// Helper.SWAP(ref p01, ref p02);
+            ///
+            /// Console.WriteLine($"P01 = {p01}");
+            /// Console.WriteLine($"P02 = {p02}");  
+           
+            
+            #endregion
+
         }
     }
 }
