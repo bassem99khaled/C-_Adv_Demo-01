32bbd99e0dd2a47890adc9fe6a93ec04bfcc5e84
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-20 16:48:10 +0200

    Q 05

---
+++ FixedSizeList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Adv_Demo_01
+{
+    class FixedSizeList<T>
+    {
+        private T[] items;
+        private int count;
+
+        public int Capacity { get; private set; }
+        public int Count => count;
+
+        
+        public FixedSizeList(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            Capacity = capacity;
+            items = new T[capacity];
+            count = 0;
+        }
+
+    
+        public void Add(T item)
+        {
+            if (count >= Capacity)
+            {
+                throw new InvalidOperationException();
+            }
+
+            items[count] = item;
+            count++;
+        }
+
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return items[index];
+        }
+
+     
+        public override string ToString()
+        {
+            return $"FixedSizeList: [{string.Join(", ", items, 0, count)}], Capacity: {Capacity}, Count: {Count}";
+        }
+    }
+}
