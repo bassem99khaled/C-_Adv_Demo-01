53bf13dd82c288bbab9dab47bc236e5cd103324b
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-20 16:43:14 +0200

    Q 01

---
+++ OptimizedBubbleSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Adv_Demo_01
+{
+    internal class OptimizedBubbleSort
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            bool swap;
+
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swap = false;
+
+
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swap = true;
+                    }
+                }
+
+
+                if (!swap)
+                {
+                    break;
+                }
+            }
+        }
+
+
+        public static void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
